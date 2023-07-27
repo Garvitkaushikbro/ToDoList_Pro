a4199e9d355eb0c0480309aab045050702053ba8
Garvitkaushikbro <garvitkaushik123@gmail.com>
2023-07-27 16:49:52 +0530

    Add search functionality

--- scripts/addFunctionality.js
+++ scripts/addFunctionality.js
@@ -1,8 +1,12 @@
 const operations = document.querySelector(".operations");
 const modal = document.querySelector(".modal");
+const modalSearch = document.querySelector(".modalSearch");
 const overlay = document.querySelector(".overlay");
+const overlaySearch = document.querySelector(".overlaySearch");
 const task_form = document.querySelector("#form_task");
+const taskFormSearch = document.querySelector("#form_task_search");
 const closeModal = document.querySelector(".close_form_task");
+const closeModalSearch = document.querySelector(".close_form_task_search");
 
 const removeModal = function () {
   overlay.classList.add("hidden");
@@ -14,9 +18,23 @@ const showModal = function () {
   modal.classList.remove("hidden");
 };
 
+const removeModalSearch = function () {
+  overlaySearch.classList.add("hidden");
+  modalSearch.classList.add("hidden");
+};
+
+const showModalSearch = function () {
+  overlaySearch.classList.remove("hidden");
+  modalSearch.classList.remove("hidden");
+};
+
 overlay.addEventListener("click", removeModal);
 
+overlaySearch.addEventListener("click", removeModalSearch);
+
 closeModal.addEventListener("click", removeModal);
+
+closeModalSearch.addEventListener("click", removeModalSearch);
 
 operations.addEventListener("click", function (e) {
   // event delegation
@@ -27,6 +45,7 @@ operations.addEventListener("click", function (e) {
   if (target.classList.contains("add")) {
     showModal();
   } else if (target.classList.contains("search")) {
+    showModalSearch();
   }
 });
 
@@ -85,5 +104,23 @@ task_form.addEventListener("submit", function (event) {
   task_form.reset();
 
   removeModal();
+  render(newData, showAll);
+});
+
+taskFormSearch.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let newData = JSON.parse(localStorage.getItem("data"));
+  newData = newData.map((obj) => {
+    obj.dueDate = new Date(obj.dueDate);
+    return obj;
+  });
+  const keyword = taskFormSearch.elements.form_task_search.value;
+  newData = newData.filter((d) => {
+    if (d.title.toLowerCase() === keyword.toLowerCase()) return true;
+    if (d.description.toLowerCase() === keyword.toLowerCase()) return true;
+    if (d.tags.includes(keyword)) return true;
+    return false;
+  });
+  removeModalSearch();
   render(newData, showAll);
 });
